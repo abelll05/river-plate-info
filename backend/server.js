8922ee85a74491e7e546324a80ee442107007ff6
abelll05 <abelmerc45@gmail.com>
2024-12-03 18:18:38 -0300

    Mensaje de commit

--- backend/server.js
+++ backend/server.js
@@ -24,12 +24,12 @@ const corsOptions = {
 app.use(cors(corsOptions)); // Usamos corsOptions aquí
 
 // Middlewares
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json()); // Para parsear el cuerpo de la solicitud como JSON
+app.use(cookieParser()); // Para parsear las cookies
 
 // Solo loggers en desarrollo
 if (process.env.NODE_ENV !== "production") {
-  app.use(morgan("dev"));
+  app.use(morgan("dev")); // Logger en consola durante el desarrollo
 }
 
 // Rutas de autenticación
@@ -43,47 +43,47 @@ app.get("/", (req, res) => {
 // Ruta para obtener la lista de jugadores
 app.get("/api/players", (req, res) => {
   const players = [
-      // Arqueros
-      { name: "Franco Armani", position: "Arquero" },
-      { name: "Jeremías Ledesma", position: "Arquero" },
-      { name: "Lucas Lavagnino", position: "Arquero" },
-      { name: "Santiago Beltrán", position: "Arquero" },
-      // Defensores
-      { name: "Ramiro Funes Morí", position: "Defensor" },
-      { name: "Federico Gattoni", position: "Defensor" },
-      { name: "Enzo Diaz", position: "Defensor" },
-      { name: "Leandro Gonzalez Pirez", position: "Defensor" },
-      { name: "Fabricio Bustos", position: "Defensor" },
-      { name: "Paulo Diaz", position: "Defensor" },
-      { name: "Milton Casco", position: "Defensor" },
-      { name: "Daniel Zabala", position: "Defensor" },
-      { name: "Marcos Acuña", position: "Defensor" },
-      { name: "Agustín Santana", position: "Defensor" },
-      { name: "German Pezzela", position: "Defensor" },
-      // Volantes
-      { name: "Nicolas Fonseca", position: "Volante" },
-      { name: "Matias Kranevitter", position: "Volante" },
-      { name: "Manuel Lanzini", position: "Volante" },
-      { name: "Gonzalez Martínez", position: "Volante" },
-      { name: "Claudio Echeverri", position: "Volante" },
-      { name: "Rodrigo Villagra", position: "Volante" },
-      { name: "Ignacio Fernández", position: "Volante" },
-      { name: "Rodrigo Aliendro", position: "Volante" },
-      { name: "Franco Mastantuono", position: "Volante" },
-      { name: "Santiago Simón", position: "Volante" },
-      { name: "Jonas Luna", position: "Volante" },
-      { name: "Tobias Leiva", position: "Volante" },
-      { name: "Santiago Lencina", position: "Volante" },
-      // Delanteros
-      { name: "Adam Bareiro", position: "Delantero" },
-      { name: "Maximiliano Meza", position: "Delantero" },
-      { name: "Miguel Borja", position: "Delantero" },
-      { name: "Facundo Colidio", position: "Delantero" },
-      { name: "Agustín Ruberto", position: "Delantero" },
-      { name: "Pablo Solari", position: "Delantero" },
-      { name: "Ian Suriabre", position: "Delantero" },
-      { name: "Tomas Nasif", position: "Delantero" },
-    ];
+    // Arqueros
+    { name: "Franco Armani", position: "Arquero" },
+    { name: "Jeremías Ledesma", position: "Arquero" },
+    { name: "Lucas Lavagnino", position: "Arquero" },
+    { name: "Santiago Beltrán", position: "Arquero" },
+    // Defensores
+    { name: "Ramiro Funes Morí", position: "Defensor" },
+    { name: "Federico Gattoni", position: "Defensor" },
+    { name: "Enzo Diaz", position: "Defensor" },
+    { name: "Leandro Gonzalez Pirez", position: "Defensor" },
+    { name: "Fabricio Bustos", position: "Defensor" },
+    { name: "Paulo Diaz", position: "Defensor" },
+    { name: "Milton Casco", position: "Defensor" },
+    { name: "Daniel Zabala", position: "Defensor" },
+    { name: "Marcos Acuña", position: "Defensor" },
+    { name: "Agustín Santana", position: "Defensor" },
+    { name: "German Pezzela", position: "Defensor" },
+    // Volantes
+    { name: "Nicolas Fonseca", position: "Volante" },
+    { name: "Matias Kranevitter", position: "Volante" },
+    { name: "Manuel Lanzini", position: "Volante" },
+    { name: "Gonzalez Martínez", position: "Volante" },
+    { name: "Claudio Echeverri", position: "Volante" },
+    { name: "Rodrigo Villagra", position: "Volante" },
+    { name: "Ignacio Fernández", position: "Volante" },
+    { name: "Rodrigo Aliendro", position: "Volante" },
+    { name: "Franco Mastantuono", position: "Volante" },
+    { name: "Santiago Simón", position: "Volante" },
+    { name: "Jonas Luna", position: "Volante" },
+    { name: "Tobias Leiva", position: "Volante" },
+    { name: "Santiago Lencina", position: "Volante" },
+    // Delanteros
+    { name: "Adam Bareiro", position: "Delantero" },
+    { name: "Maximiliano Meza", position: "Delantero" },
+    { name: "Miguel Borja", position: "Delantero" },
+    { name: "Facundo Colidio", position: "Delantero" },
+    { name: "Agustín Ruberto", position: "Delantero" },
+    { name: "Pablo Solari", position: "Delantero" },
+    { name: "Ian Suriabre", position: "Delantero" },
+    { name: "Tomas Nasif", position: "Delantero" },
+  ];
 
   res.json(players);
 });
